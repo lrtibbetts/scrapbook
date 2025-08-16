9936f998458463fea6895cdc413aa4c4dded0d94
Lucy Tibbetts <lucilletibbetts@gmail.com>
2025-08-16 14:47:04 -0700

    adjust map style

--- src/app/map.tsx
+++ src/app/map.tsx
@@ -8,26 +8,38 @@ export default function Map() {
     const map = useRef<mapboxgl.Map | null>(null);
 
     useEffect(() => {
-        if (map.current) return;
+        if (map.current || !mapContainer.current) return;
 
         mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
         
-        if (mapContainer.current) {
-            map.current = new mapboxgl.Map({
-              container: mapContainer.current,
-              style: 'mapbox://styles/mapbox/outdoors-v12',
-              center: [12.1357, 46.5369], // Cortina d'Ampezzo, Italy
-              zoom: 9
-            });
-          }
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/standard-satellite',
+            center: [12.0057, 46.5180], // Passo Falzarego, Italy
+            zoom: 12
+        });
+
+        map.current.on('style.load', () => {
+            map.current?.setConfig(
+                'basemap',
+                {
+                    lightPreset: 'dawn',
+                    showRoadsAndTransit: false,
+                    showRoadLabels: false,
+                    showTransitLabels: false,
+                    showLandBoundaries: false,
+                    showAdminBoundaries: false,
+                    font: 'Roboto Mono'
+                }
+            );
+        });
     }, []);
 
     return (
         <div>
-            <p>The Dolomites</p>
             <div 
                 ref={mapContainer} 
-                style={{ width: '100%', height: '400px'}}
+                style={{ width: '100%', height: '100vh'}}
             />
         </div>
     );
